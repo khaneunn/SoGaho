560a76d2c6a890a076576bb7886afafec54d6f9f
Eunitah <ekenyan@gmail.com>
2022-06-05 21:04:39 +0300

    all-changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,47 @@
 $(document).ready(function(){
+
+    /*--- SEARCH ---*/
+    var searchForm = $('.main-search'),
+        searchTrigger = $('.btn-search'),
+        coverLayer = $('.cover-layer');
+
+    function closeSearchForm() {
+        searchTrigger.removeClass('search-form-visible');
+        searchForm.removeClass('is-visible');
+        coverLayer.removeClass('search-form-visible');
+    }
+
+    searchTrigger.on('click', function(event){
+        event.preventDefault();
+        if( searchTrigger.hasClass('search-form-visible') ) {
+            searchForm.find('form').submit();
+        } else {
+            searchTrigger.addClass('search-form-visible');
+            coverLayer.addClass('search-form-visible');
+            searchForm.addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+                searchForm.find('input[type="search"]').focus().end().off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+            });
+        }
+    });
+
+//close search form
+    searchForm.on('click', '.btn-search-close', function(event){
+        closeSearchForm();
+        event.preventDefault();
+    });
+
+    coverLayer.on('click', function(){
+        closeSearchForm();
+    });
+
+    $(document).keyup(function(event){
+        if( event.which=='27' ){
+            closeSearchForm();
+        }
+    });
+
+
+
 $(".slider").owlCarousel({
     autoplay:false,
     loop:true,
@@ -47,6 +90,33 @@ $(".slider").owlCarousel({
             },
             1000:{
                 items:1,
+                nav:true,
+            }
+        }
+    });
+
+    $(".testing").owlCarousel({
+        autoplay:false,
+        loop:true,
+        items:1,
+        margin:25,
+        dots: true,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            650:{
+                items:1,
+                nav:true,
+            },
+            768:{
+                items:1,
+                nav:true,
+            },
+            1000:{
+                items:3,
                 nav:true,
             }
         }
