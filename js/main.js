3302ca0f7457c17ed6686606931b933ddf19ab71
Eunitah <ekenyan@gmail.com>
2022-07-06 10:44:10 +0300

    Responsiveness

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ $(document).ready(function(){
             nav:true,
         },
         768:{
-            items:2,
+            items:1,
             nav:true,
         },
         1000:{
@@ -193,6 +193,45 @@ $(document).ready(function(){
                 });
             });
         }
+    });
+
+
+    /*--- Mobile Menu ---*/
+    $('.menu-icon').bind('touchstart mousedown', function(e){
+        $(".mobile-nav li").removeClass("open");
+        $(".sub-nav").slideUp(200);
+        $("body").toggleClass("menu-open");
+        if($("body").hasClass("search-open")){
+            $("body").removeClass("search-open");
+        }
+        return false;
+    });
+
+    $(".has-children .toggle").click(function(e) {
+        var sub_nav = $(this).closest("li").children(".sub-nav");
+        if(sub_nav.is(':visible'))
+        {
+            $(this).closest("li").removeClass("open");
+            sub_nav.slideUp(200);
+        }
+        else
+        {
+            $(".mobile-nav li").removeClass("open");
+            $(this).closest("li").addClass("open");
+            $(".sub-nav").slideUp(200);
+            sub_nav.slideDown(200);
+        }
+
+        return false;
+
+    });
+
+    $("#content_mask, .menu-close").bind('touchstart mousedown', function(e){
+        $(window).scrollTop(0);
+        $(".mobile-nav li").removeClass("open");
+        $(".sub-nav").slideUp(200);
+        $("body").removeClass("menu-open search-open");
+        return false;
     });
 
 });
